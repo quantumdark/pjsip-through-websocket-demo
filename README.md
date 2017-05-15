0b77c114c4f13effc582df4c8ad51c176a3d8e9c
Yegor Dolgopolov <quantumdark@gmail.com>
2017-05-15 11:04:52 +0300

    Logging, JSONRPC protocol

--- README.md
+++ README.md
@@ -6,35 +6,20 @@ A small demo to demonstrate managing the softphone pjsua from javascript through
 1) Build pjproject and pjsua-python module and install it  
 2) git clone https://github.com/dpallot/simple-websocket-server.git  
 3) install simple-websocket-server
+4) install packages from req.txt
 
 # How to try
-1) start the server ./websocketsip.py sipserver login password  
-2) open websocket.html from the project simple-websocket-server  
+0) Check websocketsip.conf
+1) start the server ./websocketsip.py
+2) open debug.html
 3) Try commands, you'll see something like this:  
-  
-response: Connected to server. SIP Account={Account \<sip:1111111@sip.------.com\>}  
-sent: make_call sip:111111111111@sip.------.com  
-response: Calling make_call sip:111111111111@sip.------.com...  
-response: Call is CALLING, last code=0 ()  
-response: Call is EARLY, last code=183 (Session Progress)  
-response: Hello world, I can talk!  
-response: Call is EARLY, last code=180 (Ringing)  
-response: Call is CONNECTING, last code=200 (OK)  
-response: Call is CONFIRMED, last code=200 (OK)  
-response: Hello world, I can talk!  
-sent: hold  
-response: Call is DISCONNCTD, last code=200 (Normal call clearing)  
-sent: hold  
-response: You have no call  
-sent: make_call sip:111111111111@sip.------.com  
-response: Calling make_call sip:111111111111@sip.------.com...  
-response: Call is CALLING, last code=0 ()  
-response: Call is EARLY, last code=183 (Session Progress)  
-response: Hello world, I can talk!  
-response: Call is EARLY, last code=180 (Ringing)  
-response: Call is CONNECTING, last code=200 (OK)  
-response: Call is CONFIRMED, last code=200 (OK)  
-response: Hello world, I can talk!  
-sent: hangup  
-response: Call is DISCONNCTD, last code=603 ()  
-disconnected  
+
+connected
+response: {"jsonrpc": "2.0", "params": {"account": "{Account <sip:111@sip.zadarma.com>}"}, "method": "connected"}
+sent:  {"jsonrpc": "2.0", "id": 3, "method": "make_call", "params": {"uri": "oflfd1223"}}
+response: {"jsonrpc": "2.0", "id": 3, "error": {"message": "Invalid params", "code": -32602, "data": {"message": "make_call() got an unexpected keyword argument 'uri'", "args": ["make_call() got an unexpected keyword argument 'uri'"], "type": "TypeError"}}}
+sent:  {"jsonrpc": "2.0", "id": 3, "method": "make_call", "params": {"sip_uri": "oflfd1223"}}
+response: {"jsonrpc": "2.0", "id": 3, "error": {"message": "Server error", "code": -32000, "data": {"message": "global name 'uri' is not defined", "args": ["global name 'uri' is not defined"], "type": "NameError"}}}
+sent:  {"jsonrpc": "2.0", "id": 4, "method": "hangup"}
+response: {"jsonrpc": "2.0", "result": false, "id": 4}
+
--- websocketsip.py
+++ websocketsip.py
@@ -1,21 +1,60 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
 import sys
+import ConfigParser as cf
 import signal
 import pjsua as pj
+import logging
+import logging.config
 from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
+from jsonrpc import JSONRPCResponseManager, dispatcher
+from jsonrpc.jsonrpc2 import JSONRPC20Request
+
+logging.config.fileConfig('logging.conf')
+logger = logging.getLogger('wssip')
 
 
-# Logging callback
-def log_cb(level, str, len):
-    print str,
+CONFIGFILE = 'websocketsip.conf'
+
+
+
+
+try:
+    config = cf.ConfigParser()
+    config.readfp(open(CONFIGFILE))
+    config.read(['site.cfg', os.path.expanduser('~/.myapp.cfg')])
+except IOError:
+    logger.error('Failed to load config file: %s' % CONFIGFILE)
+    sys.exit(-1)
+try:
+    server = config.get('SIP', 'server')
+    login = config.get('SIP', 'login')
+    password = config.get('SIP', 'password')
+    sockethost = config.get('WEBSOCKET', 'host')
+    if sockethost == '*':
+        sockethost = ''
+    socketport = config.getint('WEBSOCKET', 'port')
+except cf.NoOptionError as e:
+    logger.error(e)
+    sys.exit(-1)
+except cf.NoSectionError as e:
+    logger.error(e)
+    sys.exit(-1)
+except Exception as e:
+    logger.exception(e)
+    sys.exit(-1)
 
 lib = None
 acc = None
 
+# Logging callback
+def log_cb(level, str, len):
+    logger.info(str)
+
 def signal_handler(signal, frame):
         global lib
-        print('You pressed Ctrl+C!')
+        logger.info('You pressed Ctrl+C!')
         # We're done, shutdown the library
         if lib:
             lib.destroy()
@@ -23,9 +62,6 @@ def signal_handler(signal, frame):
         sys.exit(0)
 signal.signal(signal.SIGINT, signal_handler)
 
-if len(sys.argv) < 4:
-    print "Use: websocketsip.py <sipserver> <login> <password>"
-    sys.exit(0)
 
 try:
     lib = pj.Lib()
@@ -40,20 +76,44 @@ try:
 
     # Create local/user-less account
     try:
-        acc_cfg = pj.AccountConfig(sys.argv[1], sys.argv[2], sys.argv[3])
+        acc_cfg = pj.AccountConfig(server, login, password)
         acc = lib.create_account(acc_cfg)
 
     except pj.Error, err:
-        print 'Error creating account:', err
+        logger.info('Error creating account: %s', err)
 
 except pj.Error, e:
-    print "Exception: " + str(e)
+    logger.error("Exception: %s", str(e))
     lib.destroy()
     lib = None
     sys.exit(-1)
 
 
 class MyCallCallback(pj.CallCallback):
+    """
+    Call state variations.
+    NULL            -- call is not initialized.
+    CALLING         -- initial INVITE is sent.
+    INCOMING        -- initial INVITE is received.
+    EARLY           -- provisional response has been sent or received.
+    CONNECTING      -- 200/OK response has been sent or received.
+    CONFIRMED       -- ACK has been sent or received.
+    DISCONNECTED    -- call is disconnected.
+
+    Call media state variations.
+    NULL        -- media is not available.
+    ACTIVE      -- media is active.
+    LOCAL_HOLD  -- media is put on-hold by local party.
+    REMOTE_HOLD -- media is put on-hold by remote party.
+    ERROR       -- media error (e.g. ICE negotiation failure).
+    """
+    media_state = {
+        pj.MediaState.NULL: 'not_available',
+        pj.MediaState.ACTIVE: 'active',
+        pj.MediaState.LOCAL_HOLD: 'local_hold',
+        pj.MediaState.REMOTE_HOLD: 'remote_hold',
+        pj.MediaState.ERROR: 'error',
+    }
     def __init__(self, websocket=None, call=None):
         self.websocket = websocket
         pj.CallCallback.__init__(self, call)
@@ -63,11 +123,15 @@ class MyCallCallback(pj.CallCallback):
         s_text = self.call.info().state_text
         code = self.call.info().last_code
         reason = self.call.info().last_reason
-        print "Call is ", s_text,
-        print "last code =", code,
-        print "(" + reason + ")"
-        self.websocket.sendMessage(
-            u'Call is {}, last code={} ({})'.format(s_text, code, reason))
+        media_state = self.media_state.get(
+                     self.call.info().media_state,
+                     'unknown'
+                 )
+        notify20(self.websocket, 'call_status_update', 
+                 {'status': s_text,
+                  'code': code,
+                  'reason': reason,
+                  'media_state': media_state})
         if s_text == 'DISCONNCTD':
             # nullify call istance to prevent lib from crash
             self.websocket.call = None
@@ -80,96 +144,130 @@ class MyCallCallback(pj.CallCallback):
             call_slot = self.call.info().conf_slot
             lib.conf_connect(call_slot, 0)
             lib.conf_connect(0, call_slot)
-            self.websocket.sendMessage(u"Hello world, I can talk!")
-            print "Hello world, I can talk!"
+            s_text = self.call.info().state_text
+            code = self.call.info().last_code
+            reason = self.call.info().last_reason
+            media_state = self.media_state.get(
+                         self.call.info().media_state,
+                         'unknown'
+                     )
+            notify20(self.websocket, 'call_media_state_update', 
+                     {'status': s_text,
+                      'code': code,
+                      'reason': reason,
+                      'media_state': media_state})
 
 
-class SimpleSip(WebSocket):
+def notify20(websocket, method, params=None):
+    # Sending notifications to the clients helper function
+    try:
+        notification = unicode(
+            JSONRPC20Request(method, params, is_notification=True).json,
+            'utf-8')
+        logger.info("Sending notification: %r", notification)
+        websocket.sendMessage(notification)
+    except Exception as e:
+        logger.exception(e)
+
+
+class Dispatcher:
 
     call = None
 
-    def make_call(self, uri):
-        # Make call
-        if self.call:
-            print 'only one call supported'
-            return
-        print "calling ", uri, '...'
-        if uri.startswith('sip:'):
-            self.sendMessage(u"Calling {}...".format(self.data))
-            self.call = acc.make_call(uri.encode(), MyCallCallback(self))
-        else:
-            self.sendMessage(u"Wrong SIP id {}.".format(uri))
-
-    def hangup(self):
-        print 'called hangup call'
-        if self.call:
-            self.call.hangup()
-            self.call = None
-        else:
-            self.sendMessage(u"You have no call")
-
-    def hold(self):
-        print 'called hold call'
-        if self.call:
-            self.call.hold()
-        else:
-            self.sendMessage(u"You have no call")
-
-    def unhold(self):
-        print 'called unhold call'
-        if self.call:
-            self.call.unhold()
-        else:
-            self.sendMessage(u"You have no call")
-
-    def mute_mic(self):
-        print 'called mute mic call'
-        if not self.lib:
-            return
-        try:
-            tx_level, rx_level = self.lib.conf_get_signal_level(0)
-            if rx_level > 0.0:
-                self.lib.conf_set_rx_level(0, 0)
-                levels = self.lib.conf_get_signal_level(0)
-                self.sendMessage(u"Mic muted (level: {})".format(levels))
-            else:
-                self.lib.conf_set_rx_level(0, 1)
-                levels = self.lib.conf_get_signal_level(0)
-                self.sendMessage(u"Mic unmuted (level: {})".format(levels))
-        except Exception as e:
-            print e,
-
-    def handleMessage(self):
-        # commands in plain text like:
-        # methodname first second ...
-        # would be dispatched to self.methodname(first, second, ...)
-        # e.g.: 'sum 3.1415 666' >>> self.sum(3.1415, 666)
-        print self.address, self.data
-        if not self.lib:
-            self.sendMessage(u"Somethong wrong with SIP library")
-        if self.acc:
-            command = self.data.split()
-            # dispatch comand to self method
-            func = getattr(self, command[0], None)
-            if func:
-                func(*command[1:])
-            else:
-                self.sendMessage(u"Unknown function")
-        else:
-            self.sendMessage(u"Sip account not found")
-
-    def handleConnected(self):
-        # The Lib supports only one instance in memory
+    def __init__(self, websocket):
         global lib
         global acc
         self.lib = lib
         self.acc = acc
-        self.sendMessage(u"Connected to server. SIP Account={}".format(acc))
-        print self.address, 'connected'
+        self.websocket = websocket
+
+    def __getitem__(self, key):
+        try:
+            return getattr(self, key)
+        except AttributeError:
+            raise KeyError
+
+    def make_call(self, sip_uri):
+        logger.debug('called make_call method')
+        # Make call
+        if self.call:
+            logger.info('only one call supported')
+            return
+        logger.info("calling %s...", uri)
+        if uri.startswith('sip:'):
+            self.call = acc.make_call(
+                uri.encode(),
+                MyCallCallback(self.websocket))
+            return True
+        else:
+            raise ValueError("Wrong SIP id {}.".format(uri))
+
+    def hangup(self):
+        logger.debug('called hangup method')
+        if self.call:
+            self.call.hangup()
+            self.call = None
+            return True
+        else:
+            return False
+
+    def hold(self):
+        logger.debug('called hold method')
+        if self.call:
+            self.call.hold()
+            return True
+        else:
+            return False
+
+    def unhold(self):
+        logger.debug('called unhold method')
+        if self.call:
+            self.call.unhold()
+            return True
+        else:
+            return False
+
+    def mute_mic(self):
+        logger.debug('called mute_mic method')
+        if not self.lib:
+            return
+        tx_level, rx_level = self.lib.conf_get_signal_level(0)
+        if rx_level > 0.0:
+            self.lib.conf_set_rx_level(0, 0)
+            levels = self.lib.conf_get_signal_level(0)
+            return levels
+        else:
+            self.lib.conf_set_rx_level(0, 1)
+            levels = self.lib.conf_get_signal_level(0)
+            return levels
+
+
+class WebSocketSip(WebSocket):
+
+    def handleMessage(self):
+        logger.debug("Reseived from %s data: %s", self.address, self.data)
+        # dispatch comand
+        try:
+            response = JSONRPCResponseManager.handle(self.data, self.dispatcher)
+        except Exception as e:
+            logger.exception(e)
+        if response is not None:
+            logger.info("Request: %r, Response: %r",
+                        self.data,
+                        unicode(response.json, 'utf-8'))
+            self.sendMessage(unicode(response.json, 'utf-8'))
+        else:
+            logger.info("Notification received: %r", self.data)
+
+    def handleConnected(self):
+        # The Lib supports only one instance in memory
+        self.dispatcher = Dispatcher(self)
+        notify20(self, 'connected', {'account': str(acc)})
+        logger.info('Connected: %s', self.address)
 
     def handleClose(self):
-        print self.address, 'closed'
+        logger.info('Connection closed: %s', self.address)
 
 
-server = SimpleWebSocketServer('', 8000, SimpleSip)
+server = SimpleWebSocketServer(sockethost, socketport, WebSocketSip)
 server.serveforever()
